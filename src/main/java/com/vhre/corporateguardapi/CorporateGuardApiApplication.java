5c9a3f2a84f6dd2a3360ddd696ff840adfd1d6c6
Victor Hugo Romero Esquivel <vh.romero.e@gmail.com>
2026-02-09 22:00:28 -0600

    first commi

---
+++ src/main/java/com/vhre/corporateguardapi/CorporateGuardApiApplication.java
@@ -0,0 +1,13 @@
+package com.vhre.corporateguardapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorporateGuardApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CorporateGuardApiApplication.class, args);
+    }
+
+}
